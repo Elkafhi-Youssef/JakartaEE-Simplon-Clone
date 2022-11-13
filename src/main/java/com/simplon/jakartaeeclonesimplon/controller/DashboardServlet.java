6b267930130ac44bb03a8132cecf28ec0abdda78
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-11-13 17:37:34 +0100

    SCBF-35 dashboard admin (student && their promo)

---
+++ src/main/java/com/simplon/jakartaeeclonesimplon/controller/DashboardServlet.java
@@ -0,0 +1,33 @@
+package com.simplon.jakartaeeclonesimplon.controller;
+
+import com.simplon.jakartaeeclonesimplon.entity.Students;
+import com.simplon.jakartaeeclonesimplon.service.StudentService;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "DashboardServlet", value = "/DashboardServlet")
+public class DashboardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        StudentService studentService  = new StudentService();
+
+        List<Students> students = new ArrayList<Students>();
+        students = studentService.getAllStudents();
+        for (Students student : students) {
+            System.out.println(student.getStudentId());
+
+        }
+        request.setAttribute("studentsList", students);
+        request.getRequestDispatcher("admin/dashboard.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
